1d0bef90eb7186da3cf9eb0926cf4443292de3de
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-19 11:28:57 -0500

    Add files via upload

---
+++ ejercicio4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace miEjercicio4
+{
+    class Program
+    {
+        static void Main(string[] args )
+        { 
+            
+           // int d = 100 + 50;
+
+            double a= 3.5 + 4.75;
+
+           // Console.WriteLine(d);
+
+            Console.WriteLine(a);
+        }
+    }
+}

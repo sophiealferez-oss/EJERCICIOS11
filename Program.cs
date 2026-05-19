ecb9dc8c29830efc1fc2e5e1df9791e90dc28eae
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-19 11:01:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Miejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble= 9.78;
+            int myInt= (int) myDouble;
+
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myInt);
+        }
+    }
+}

8f8becb9c050216a124164c9a70a2bf8d56298ec
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-19 11:29:44 -0500

    Add files via upload

---
+++ ejercicio5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace miEjercicio4
+{
+    class Program
+    {
+        static void Main(string[] args )
+        { 
+           int sum1 = 100+50;
+           int sum2 = sum1+250;
+           int sum3 = sum2+sum2;
+           
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine(sum3);
+        }
+    }
+}

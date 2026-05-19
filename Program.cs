5ef04dfa78c5ed76ae04108bb65178790e512d47
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-19 11:03:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Miejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 10;
+            Console.WriteLine(Convert.ToString(myInt));
+        }
+    }
+}

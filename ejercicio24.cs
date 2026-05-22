d1fe158ca1d238480eea561b9bf357c17fd9f1df
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 15:41:12 -0500

    Add files via upload

---
+++ ejercicio24.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ejercicio24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //full name
+            string name = "Shirley Alvarez";
+
+            //Location of the letter i
+            int charPos = name.IndexOf("i");
+
+            //Get last name
+            string lastName =name.Substring(charPos);
+
+            //print the result
+            Console.WriteLine(lastName);
+;        }
+    }
+}
+
+
+

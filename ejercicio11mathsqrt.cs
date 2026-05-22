a1332ae321d838a87044d5cb365e898c6ae151e8
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 10:29:38 -0500

    Add files via upload

---
+++ ejercicio11mathsqrt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ejercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(64))
+;        }
+    }
+}
+

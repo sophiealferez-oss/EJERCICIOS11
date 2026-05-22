5f1562f2c14640eff6cd16ea782bb282ca375212
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 15:24:01 -0500

    Add files via upload

---
+++ ejercicio23.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ejercicio23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre = "john ";
+            string apellido= "ruiz";
+            string ncompleto = $"My full name is: {nombre} {apellido}"; //otra manera de concatenar:)
+
+            Console.WriteLine(ncompleto);
+;        }
+    }
+}
+
+
+
+

dbf6a05734d8d5cac61907589e8ac863d0cd2fec
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 15:18:59 -0500

    Add files via upload

---
+++ ejercicio21.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ejercicio21
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string x = "10";
+            string y= "20";
+            string z = x+y;
+
+            Console.WriteLine(z);
+;        }
+    }
+}
+
+
+
+

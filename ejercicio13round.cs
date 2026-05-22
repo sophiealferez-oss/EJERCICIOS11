770faa51dcd55886d2e32d31f83c50cb0740755a
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 10:36:06 -0500

    Add files via upload

---
+++ ejercicio13round.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(9.99))
+;        }
+    }
+}
+

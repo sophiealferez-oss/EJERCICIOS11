63bf6cff54089c9771c7f6b00595760c3376909e
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 11:01:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "hello world";
+            Console.WriteLine(txt.ToUpper());
+             Console.WriteLine(txt.ToLower());
+
+;        }
+    }
+}
+
+

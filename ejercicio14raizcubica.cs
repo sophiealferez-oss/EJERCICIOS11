8f80db2599771b65a9e6648276b7acd22fd5573b
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 10:45:40 -0500

    Add files via upload

---
+++ ejercicio14raizcubica.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+namespace ejercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero = 27.0;
+            double resultado = Math.Cbrt(numero);
+            Console.WriteLine($"La raíz cúbica de {numero} es: {resultado}");
+;        }
+    }
+}
+

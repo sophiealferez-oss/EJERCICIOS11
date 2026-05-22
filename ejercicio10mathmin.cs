0f9d2b6ea59f5a9f0be05125c2f3c85e6b8cbe16
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 10:25:48 -0500

    Add files via upload

---
+++ ejercicio10mathmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ejercicio10
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Min(5, 10))
+;        }
+    }
+}
+

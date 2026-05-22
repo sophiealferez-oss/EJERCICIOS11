8a492e880d3a7c05c11511f3cd6ea9c87b0aa146
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 15:35:57 -0500

    Add files via upload

---
+++ ejercicioposicion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ejercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString = "Hallo";
+            Console.WriteLine(myString[0]);
+;        }
+    }
+}
+
+
+

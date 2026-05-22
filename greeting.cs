17d9ae0549973f273038c4d73682b5ebaf79cfcb
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 15:58:57 -0500

    Add files via upload

---
+++ greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hallo, ";
+            string greeting2 = "it is very nice to meet you";
+            string greetings = greeting + greeting2;
+            Console.WriteLine(greetings)
+;        }
+    }
+}
+
+

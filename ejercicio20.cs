47f83e776798f41eea1753ff8eb6e3d61f5f05a7
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 15:15:22 -0500

    Add files via upload

---
+++ ejercicio20.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+namespace ejercicio20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10; 
+            int y = 20;
+            int z= x + y;
+
+            Console.WriteLine(z);
+;        }
+    }
+}
+
+
+

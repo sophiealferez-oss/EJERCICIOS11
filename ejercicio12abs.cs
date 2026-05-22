d10aa7d7f59952d64388b532e4845397c285c397
sophiealferez-oss <sophiealferez@gmail.com>
2026-05-22 10:33:14 -0500

    Add files via upload

---
+++ ejercicio12abs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(-4.7));
+      }
+    }
+}
+

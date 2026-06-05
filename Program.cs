18afe9e319ac59adb842c49b6788eb167cf407f2
sophiealferez-oss <sophiealferez@gmail.com>
2026-06-05 00:17:50 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Myapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "We are the so-called \"Vikings\" from the north.";
+            Console.WriteLine(txt);
+        }
+    }
+}

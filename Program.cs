f59e9cb8bdc97a6ed034b2f450ecb9366fa95348
sophiealferez-oss <sophiealferez@gmail.com>
2026-06-05 00:33:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[,] numbers = { {1, 4, 2}, {3, 6, 8} };
+    
+      for (int i = 0; i < numbers.GetLength(0); i++) 
+      {  
+        for (int j = 0; j < numbers.GetLength(1); j++) //esto es para especificar cuántas veces debe ejecutarse el bucle.
+        {  
+          Console.WriteLine(numbers[i, j]);  
+        }  
+      }  
+    }
+  }
+}

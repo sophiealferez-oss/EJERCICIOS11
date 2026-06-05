f6ab5d5127b1514c5d5daefc9d35f0db1c50c333
sophiealferez-oss <sophiealferez@gmail.com>
2026-06-05 00:15:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string txt = "jhgfds";
+            Console.WriteLine(" the length of the txt string is: " + txt.Length);
+;        }
+    }
+}
+
+

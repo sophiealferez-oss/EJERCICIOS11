895579bc82f42141717877f175f5a22e74e3790e
sophiealferez-oss <sophiealferez@gmail.com>
2026-06-05 00:32:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int i = 0;
+      while (i < 10) 
+      {
+        Console.WriteLine(i);
+        i++;
+        if (i == 4) 
+        {
+          break; //para interrumpir y continuar con el bucle
+        }
+      }    
+    }
+  }
+}
